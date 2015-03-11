5422e5d357d7de3a21e90fb0d18015409408c2ba
l3gend <j.ant.wallace@gmail.com>
2015-03-11 13:27:31 -0500

    added icons to scopes

--- scripts/util/mbsutility.js
+++ scripts/util/mbsutility.js
@@ -316,6 +316,9 @@ function getUserShop($scope) {
 }
 
 function logOut() {
+    if (window.AdMob && hasAds)
+        window.AdMob.removeBanner();
+
     clearUserData();
 }
 
@@ -364,6 +367,50 @@ function getUserData($scope) {
         $scope.mbsBarberID = localStorage["mbsBarberID"];
     if (true)
         $scope.hasAds = true;
+
+    var iconPrefix = "icon ";
+
+    if(ionic.Platform.isIOS())
+    {
+        $scope.menuIcon = iconPrefix + "ion-navicon";
+        $scope.searchIcon = iconPrefix + "ion-ios-search";
+        $scope.emailIcon = iconPrefix + "ion-ios-email";
+        $scope.profileIcon = iconPrefix + "ion-ios-person";
+        $scope.galleryIcon = iconPrefix + "ion-ios-film";
+        $scope.logOutIcon = iconPrefix + "ion-log-out";
+        $scope.homeIcon = iconPrefix + "ion-ios-home";
+        $scope.backIcon = iconPrefix + "ion-ios-arrow-back";
+        $scope.barberIcon = iconPrefix + "ion-ios-personadd";
+        $scope.appointmentIcon = iconPrefix + "ion-ios-calendar";
+        $scope.imageUploadIcon = iconPrefix + "ion-ios-upload-outline";
+    }else
+        if(ionic.Platform.isIOS())
+        {
+            $scope.menuIcon = iconPrefix + "ion-android-menu";
+            $scope.searchIcon = iconPrefix + "ion-android-search";
+            $scope.emailIcon = iconPrefix + "ion-android-mail";
+            $scope.profileIcon = iconPrefix + "ion-anroid-person";
+            $scope.galleryIcon = iconPrefix + "ion-android-film";
+            $scope.logOutIcon = iconPrefix + "ion-android-exit";
+            $scope.homeIcon = iconPrefix + "ion-android-home";
+            $scope.backIcon = iconPrefix + "ion-android-arrow-back";
+            $scope.barberIcon = iconPrefix + "ion-android-person-add";
+            $scope.appointmentIcon = iconPrefix + "ion-android-calendar";
+            $scope.imageUploadIcon = iconPrefix + "ion-android-upload";
+        }else
+        {
+            $scope.menuIcon = iconPrefix + "ion-navicon";
+            $scope.searchIcon = iconPrefix + "ion-search";
+            $scope.emailIcon = iconPrefix + "ion-email-unread";
+            $scope.profileIcon = iconPrefix + "ion-person";
+            $scope.galleryIcon = iconPrefix + "ion-images";
+            $scope.logOutIcon = iconPrefix + "ion-log-out";
+            $scope.homeIcon = iconPrefix + "ion-ios-home";
+            $scope.backIcon = iconPrefix + "ion-chevron-left";
+            $scope.barberIcon = iconPrefix + "ion-person-add";
+            $scope.appointmentIcon = iconPrefix + "ion-calendar";
+            $scope.imageUploadIcon = iconPrefix + "ion-upload";
+        }
 }
 
 function getBarberData($scope) {
