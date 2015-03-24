6fdaa6f8560684c089dfeb53c11d568da1e837d9
l3gend <j.ant.wallace@gmail.com>
2015-03-24 14:55:31 -0500

    height

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -510,7 +510,7 @@ angular.module('mbs.controllers', [])
                                     $scope.hasBarberShops = false;
                                     $scope.barberShopHeight = (screen.height / 2) + "px";//"250px";
 
-                                    $("#shopScroll").css("top", "-40px");
+                                    $("#shopScroll").css("top", "-80px");
                                 } else
                                 {
                                     if ($scope.currentAppointments.length == 0)
