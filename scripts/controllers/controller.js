2bf71941f68185a9d0789bd0e4b60b8da9beddd0
l3gend <j.ant.wallace@gmail.com>
2015-03-23 21:25:37 -0500

    screen height

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -506,7 +506,7 @@ angular.module('mbs.controllers', [])
                                 if ($scope.currentAppointments.length == 0 && $scope.currentBarberShops.length == 0)
                                 {
                                     $scope.hasAppointments = false;
-                                    $scope.appointmentHeight = (screen.height / 2) + "px";//"250px";
+                                    $scope.appointmentHeight = (screen.height / 3) + "px";//"250px";
                                     $scope.hasBarberShops = false;
                                     $scope.barberShopHeight = (screen.height / 2) + "px";//"250px";
                                 } else
