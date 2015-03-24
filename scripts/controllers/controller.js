6c2f2d919b5b9a39598fce9881868f099158e7e8
l3gend <j.ant.wallace@gmail.com>
2015-03-23 20:37:44 -0500

    checking screen height

--- scripts/controllers/controller.js
+++ scripts/controllers/controller.js
@@ -467,6 +467,7 @@ angular.module('mbs.controllers', [])
 	       toggleIonicLoading($ionicLoading, "Loading information ...", true, false, "positive");
 	       //showCordovaLoading($cordovaSpinnerDialog, "Loading", "Loading information ...", true, true);
 	       getUserData($scope);
+           alert(screen.height);
 	       $scope.viewHeight = (screen.height - (hasAds ? 50 : 0)) + "px";
            
 	       if($scope.backButtonPressed)
@@ -509,6 +510,7 @@ angular.module('mbs.controllers', [])
                                     $scope.appointmentHeight = (screen.height / 2) + "px";//"250px";
                                     $scope.hasBarberShops = false;
                                     $scope.barberShopHeight = (screen.height / 2) + "px";//"250px";
+                                    alert($scope.appointmentHeight);
                                 } else
                                 {
                                     if ($scope.currentAppointments.length == 0)
